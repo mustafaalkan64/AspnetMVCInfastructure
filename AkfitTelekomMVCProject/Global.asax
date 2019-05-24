f73be23cf7503b51db06c926add6077c7e160827
mustafa alkan <mustafaalkan64@gmail.com>
2019-05-24 23:20:58 +0300

    Add project files.

---
+++ AkfitTelekomMVCProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AkfitTelekomMVCProject.MvcApplication" Language="C#" %>
---
+++ AkfitTelekomMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AkfitTelekomMVCProject.Startup))]
+namespace AkfitTelekomMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
