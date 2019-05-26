3c26542ecdd9536add77243cb4c00ace9a4819dd
mustafa alkan <mustafaalkan64@gmail.com>
2019-05-26 11:59:40 +0300

    Unit Of Work implementation

--- AktifTelekom.DAL/Uow/IUnitOfWork.cs
+++ AktifTelekom.DAL/Uow/IUnitOfWork.cs
@@ -9,12 +9,34 @@ namespace AktifTelekom.DAL.Uow
 {
     public interface IUnitOfWork : IDisposable
     {
-        IGenericRepository<Users> UsersRepository { get; }
-        IGenericRepository<User> UserRepository { get; }
         IGenericRepository<A_MAIL> MailRepository { get; }
-        IGenericRepository<Customers> CustomersRepository { get; }
+        IGenericRepository<A_MODUL> ModulRepository { get; }
+        IGenericRepository<A_MUSTERIMODUL> MusteriModulRepository { get; }
         IGenericRepository<A_YETKI> YetkiRepository { get; }
         IGenericRepository<A_YETKIDETAY> YetkiDetayRepository { get; }
+        IGenericRepository<AGENT> AgentRepository { get; }
+        IGenericRepository<AgentCallLogs> AgentCallLogsRepository { get; }
+        IGenericRepository<AgentGroupHierarchies> AgentGroupHierarchiesRepository { get; }
+        IGenericRepository<AgentGroupHistories> AgentGroupHistoriesRepository { get; }
+        IGenericRepository<AGENTGRUP> AgentGroupRepository { get; }
+        IGenericRepository<AgentGroups> AgentGroupsRepository { get; }
+        IGenericRepository<AgentProcesses> AgentProcessesRepository { get; }
+        IGenericRepository<AgentSetting> AgentSettingRepository { get; }
+        IGenericRepository<AgentStatusLogs> AgentStatusLogsRepository { get; }
+        IGenericRepository<AgentTimeout> AgentTimeoutRepository { get; }
+        IGenericRepository<Appointments> AppointmentsRepository { get; }
+        IGenericRepository<AssignDetails> AssignDetailsRepository { get; }
+        IGenericRepository<CallHistories> CallHistoriesRepository { get; }
+        IGenericRepository<CallResults> CallResultsRepository { get; }
+        IGenericRepository<CallStatus> CallStatusRepository { get; }
+        IGenericRepository<CallStatusLogs> CallStatusLogsRepository { get; }
+        IGenericRepository<CallTypes> CallTypesRepository { get; }
+        IGenericRepository<cars> CarsRepository { get; }
+        IGenericRepository<CheckSendCalls> CheckSendCallsRepository { get; }
+        IGenericRepository<Cities> CitiesRepository { get; }
+        IGenericRepository<Customers> CustomersRepository { get; }
+        IGenericRepository<Users> UsersRepository { get; }
+        IGenericRepository<User> UserRepository { get; }
         Task Commit();
     }
 }
--- AktifTelekom.DAL/Uow/UnitOfWork.cs
+++ AktifTelekom.DAL/Uow/UnitOfWork.cs
@@ -10,13 +10,34 @@ namespace AktifTelekom.DAL.Uow
     public class UnitOfWork : IUnitOfWork, IDisposable
     {
         private readonly AktifTelekomDbContext _context;
-        private IGenericRepository<Users> _usersRepository;
-        private IGenericRepository<User> _userRepository;
-        private IGenericRepository<Customers> _customersRepository;
         private IGenericRepository<A_MAIL> _mailRepository;
+        private IGenericRepository<A_MODUL> _modulRepository;
+        private IGenericRepository<A_MUSTERIMODUL> _musteriModulRepository;
         private IGenericRepository<A_YETKI> _yetkiRepository;
         private IGenericRepository<A_YETKIDETAY> _yetkiDetayRepository;
-
+        private IGenericRepository<AGENT> _agentRepository;
+        private IGenericRepository<AgentCallLogs> _agentCallLogsRepository;
+        private IGenericRepository<AgentGroupHierarchies> _agentGroupHierarchiesRepository;
+        private IGenericRepository<AgentGroupHistories> _agentGroupHistoriesRepository;
+        private IGenericRepository<AGENTGRUP> _agentGroupRepository;
+        private IGenericRepository<AgentGroups> _agentGroupsRepository;
+        private IGenericRepository<AgentProcesses> _agentProcessesRepository;
+        private IGenericRepository<AgentSetting> _agentSettingRepository;
+        private IGenericRepository<AgentStatusLogs> _agentStatusLogsRepository;
+        private IGenericRepository<AgentTimeout> _agentTimeoutRepository;
+        private IGenericRepository<Appointments> _appointmentsRepository;
+        private IGenericRepository<AssignDetails> _assignDetailsRepository;
+        private IGenericRepository<CallHistories> _callHistoriesRepository;
+        private IGenericRepository<CallResults> _callResultsRepository;
+        private IGenericRepository<CallStatus> _callStatusRepository;
+        private IGenericRepository<CallStatusLogs> _callStatusLogsRepository;
+        private IGenericRepository<CallTypes> _callTypesRepository;
+        private IGenericRepository<cars> _carsRepository;
+        private IGenericRepository<CheckSendCalls> _checkSendCallsRepository;
+        private IGenericRepository<Cities> _citiesRepository;
+        private IGenericRepository<Customers> _customersRepository;
+        private IGenericRepository<Users> _usersRepository;
+        private IGenericRepository<User> _userRepository;
         public UnitOfWork(AktifTelekomDbContext context)
         {
             _context = context;
@@ -50,6 +71,116 @@ namespace AktifTelekom.DAL.Uow
         public IGenericRepository<A_YETKIDETAY> YetkiDetayRepository
         {
             get { return _yetkiDetayRepository ?? (_yetkiDetayRepository = new GenericRepository<A_YETKIDETAY>(_context)); }
+        }
+
+        public IGenericRepository<A_MODUL> ModulRepository
+        {
+            get { return _modulRepository ?? (_modulRepository = new GenericRepository<A_MODUL>(_context)); }
+        }
+
+        public IGenericRepository<A_MUSTERIMODUL> MusteriModulRepository
+        {
+            get { return _musteriModulRepository ?? (_musteriModulRepository = new GenericRepository<A_MUSTERIMODUL>(_context)); }
+        }
+
+        public IGenericRepository<AGENT> AgentRepository
+        {
+            get { return _agentRepository ?? (_agentRepository = new GenericRepository<AGENT>(_context)); }
+        }
+
+        public IGenericRepository<AgentCallLogs> AgentCallLogsRepository
+        {
+            get { return _agentCallLogsRepository ?? (_agentCallLogsRepository = new GenericRepository<AgentCallLogs>(_context)); }
+        }
+
+        public IGenericRepository<AgentGroupHierarchies> AgentGroupHierarchiesRepository
+        {
+            get { return _agentGroupHierarchiesRepository ?? (_agentGroupHierarchiesRepository = new GenericRepository<AgentGroupHierarchies>(_context)); }
+        }
+
+        public IGenericRepository<AgentGroupHistories> AgentGroupHistoriesRepository
+        {
+            get { return _agentGroupHistoriesRepository ?? (_agentGroupHistoriesRepository = new GenericRepository<AgentGroupHistories>(_context)); }
+        }
+
+        public IGenericRepository<AGENTGRUP> AgentGroupRepository
+        {
+            get { return _agentGroupRepository ?? (_agentGroupRepository = new GenericRepository<AGENTGRUP>(_context)); }
+        }
+
+        public IGenericRepository<AgentGroups> AgentGroupsRepository
+        {
+            get { return _agentGroupsRepository ?? (_agentGroupsRepository = new GenericRepository<AgentGroups>(_context)); }
+        }
+
+        public IGenericRepository<AgentProcesses> AgentProcessesRepository
+        {
+            get { return _agentProcessesRepository ?? (_agentProcessesRepository = new GenericRepository<AgentProcesses>(_context)); }
+        }
+
+        public IGenericRepository<AgentSetting> AgentSettingRepository
+        {
+            get { return _agentSettingRepository ?? (_agentSettingRepository = new GenericRepository<AgentSetting>(_context)); }
+        }
+
+        public IGenericRepository<AgentStatusLogs> AgentStatusLogsRepository
+        {
+            get { return _agentStatusLogsRepository ?? (_agentStatusLogsRepository = new GenericRepository<AgentStatusLogs>(_context)); }
+        }
+
+        public IGenericRepository<AgentTimeout> AgentTimeoutRepository
+        {
+            get { return _agentTimeoutRepository ?? (_agentTimeoutRepository = new GenericRepository<AgentTimeout>(_context)); }
+        }
+
+        public IGenericRepository<Appointments> AppointmentsRepository
+        {
+            get { return _appointmentsRepository ?? (_appointmentsRepository = new GenericRepository<Appointments>(_context)); }
+        }
+
+        public IGenericRepository<AssignDetails> AssignDetailsRepository
+        {
+            get { return _assignDetailsRepository ?? (_assignDetailsRepository = new GenericRepository<AssignDetails>(_context)); }
+        }
+
+        public IGenericRepository<CallHistories> CallHistoriesRepository
+        {
+            get { return _callHistoriesRepository ?? (_callHistoriesRepository = new GenericRepository<CallHistories>(_context)); }
+        }
+
+        public IGenericRepository<CallResults> CallResultsRepository
+        {
+            get { return _callResultsRepository ?? (_callResultsRepository = new GenericRepository<CallResults>(_context)); }
+        }
+
+        public IGenericRepository<CallStatus> CallStatusRepository
+        {
+            get { return _callStatusRepository ?? (_callStatusRepository = new GenericRepository<CallStatus>(_context)); }
+        }
+
+        public IGenericRepository<CallStatusLogs> CallStatusLogsRepository
+        {
+            get { return _callStatusLogsRepository ?? (_callStatusLogsRepository = new GenericRepository<CallStatusLogs>(_context)); }
+        }
+
+        public IGenericRepository<CallTypes> CallTypesRepository
+        {
+            get { return _callTypesRepository ?? (_callTypesRepository = new GenericRepository<CallTypes>(_context)); }
+        }
+
+        public IGenericRepository<cars> CarsRepository
+        {
+            get { return _carsRepository ?? (_carsRepository = new GenericRepository<cars>(_context)); }
+        }
+
+        public IGenericRepository<CheckSendCalls> CheckSendCallsRepository
+        {
+            get { return _checkSendCallsRepository ?? (_checkSendCallsRepository = new GenericRepository<CheckSendCalls>(_context)); }
+        }
+
+        public IGenericRepository<Cities> CitiesRepository
+        {
+            get { return _citiesRepository ?? (_citiesRepository = new GenericRepository<Cities>(_context)); }
         }
 
         public async Task Commit()
